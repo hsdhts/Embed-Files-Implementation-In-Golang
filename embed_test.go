90be435943c08fdcfcae4e21b86812c9348a1898
husada20 <husadahutasoit@gmail.com>
2023-08-08 16:12:14 +0700

    embed update

--- embed_test.go
+++ embed_test.go
@@ -1,6 +1,7 @@
 package golang_embed
 
 import (
+	"embed"
 	_ "embed"
 	"fmt"
 	"io/fs"
@@ -23,4 +24,20 @@ func TestByte(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+//go:embed files/a.txt
+//go:embed files/b.txt
+//go:embed files/c.txt
+var files embed.FS
+
+func TestMultipleFiles(t *testing.T) {
+	a, _ := files.ReadFile("files/a.txt")
+	fmt.Println(string(a))
+
+	b, _ := files.ReadFile("files/b.txt")
+	fmt.Println(string(b))
+
+	c, _ := files.ReadFile("files/c.txt")
+	fmt.Println(string(c))
 }
